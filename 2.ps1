9b826cc432be2877d5de30f3dfd2c8509fa677ad
jukkaprami <77621938+jukkaprami@users.noreply.github.com>
2021-11-17 09:53:14 +0200

    Create 2.ps1

---
+++ 2.ps1
@@ -0,0 +1,4 @@
+﻿# Haetaan käyttäjän tiedot
+Get-ADUser -Identity "jukka.prami.adm" -Properties "displayname"`
+ | Export-Csv -Path "C:\Users\administrator\My Documents\otsikot.csv"`
+  -Encoding Unicode -Delimiter ";"
