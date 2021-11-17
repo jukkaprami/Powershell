27af0afe89361ba29b16a74548d2b4c8d31e412a
jukkaprami <77621938+jukkaprami@users.noreply.github.com>
2021-11-17 09:53:01 +0200

    Create ad.ps1

---
+++ ad.ps1
@@ -0,0 +1,10 @@
+﻿# käyttäjän luominen skriptin avulla
+
+# Salasanaa ei voi antaa selkokielisenä
+$kryptattusalasana = ConvertTo-SecureString "Q1werty" -AsPlainText -Force
+
+# luodaan tili
+New-ADUser -GivenName "jukka" -SurName "prami.adm" -Name "jukka prami admin" -DisplayName "jukka prami (admin)" -SamAccountName "jukka.prami.adm" -UserPrincipalName "jukka.prami.adm@firma.intra" -Description "jukka pramin pääkäyttäjätunnus" -ChangePasswordAtLogon 1 -Enabled 1 -AccountPassword $salasana
+
+# Lisätään käyttäjä Domain admins -ryhmään
+Add-ADGroupMember -Identity  "Domain Admins" -Members "jukka.prami.adm"
