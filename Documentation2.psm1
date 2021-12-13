936e4ae0d2626eb5b10b668d9d7279af4c347ae9
jukkaprami <77621938+jukkaprami@users.noreply.github.com>
2021-12-13 12:04:11 +0200

    Add files via upload

---
+++ Documentation2.psm1
@@ -0,0 +1,93 @@
+﻿# Komentosovelma (comandlet), joka selvittää koneen verkkokortin tiedot
+
+# Luokka tulosobjektien malliksi
+class NicInfo
+{
+    [string]$Host # Tietokoneen nimi
+    [string]$NICName # Verkkokortin nimi
+    [string]$AdapterType # Verkkokortin tyyppi
+    [string]$MACAddress # Fyysinen osoite
+    [string[]]$IPAddress # Looginen osoite
+    [bool]$DHCPEnabled # Osoite saatu DHCP-palvelimelta
+    [uint64]$Speed # Verkkokortin nopeus Mb/s
+}
+
+
+
+# Esittelyfunktio, jossa määritellään komentosovelman parametrit, näkyy ulospäin (public)
+function Get-EthernetNIC # Nimi muodostetaan sääntöjen mukaan: Verbi-Objekti
+{
+    [CmdletBinding()] # Laajennettuparametrimäärittely 
+    param
+    (
+        # Parametri on pakollinen ja  se voi saada arvonsa putkittamalla
+        [Parameter(Mandatory=1, ValueFromPipeline=1, ValueFromPipelineByPropertyName=1)]
+        [String[]]$ComputerName # parametri on määritelty merkkijonovektoriksi -> arvona voi olla useita koneita 
+    )
+
+    # Lohko jossa määritellään ulospäin näkymätön (private) työfunktio
+    BEGIN 
+    {
+        # Työfunktio, jonka avulla tarvittavat tiedot hankitaan
+        function ScanNics
+        {
+            # Työfunktion parametrit
+            param([string]$ComputerName)
+
+
+            # Tyhjä vektori tuloksia varten
+            $NICs = @()
+
+            # Haetaan koneen Ethernet-verkkokortit
+            $EthernetCards = Get-WmiObject -Class  Win32_NetworkAdapter -ComputerName $ComputerName | Where-Object {$_.AdapterType -Match "Ethernet"}
+
+            # Käydään löydetyt verkkokortit yksitellen läpi
+            foreach($EthernetCard in $EthernetCards)
+            {
+                $CardId = $EthernetCard.DeviceID # Verkkokortin numerotunniste
+
+                # Haetaan verkkokortin IP-asetustiedot
+                $IPInfo = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -ComputerName $ComputerName | Where-Object {$_.Index -eq $CardId}
+
+                # Luodaan uusi NicIfo-objekti ja määritellään sen ominaisuudet
+                $NicData = [NicInfo]::new()
+                $NicData.Host = $ComputerName
+                $NicData.NICName = $EthernetCard.Name
+                $NicData.AdapterType =$EthernetCard.AdapterType
+                $NicData.MACAddress = $EthernetCard.MACAddress
+                $NicData.Speed = $EthernetCard.Speed / 1000000
+                $NicData.IPAddress = $IPInfo.IPAddress
+                $NicData.DHCPEnabled = $IPInfo.DHCPEnabled
+
+                # Lisätään objekti vektoriin
+                $NICs = $NICs + $NicData
+            }
+ 
+            # Tulostetaan tiedot
+            Write-Output $NICs
+        }
+    
+    }
+
+    # Lohko, jossa kutsutaan työfunktiota ScanNics esittelyfunktion parametrina annetuissa koneissa
+    PROCESS 
+    {
+        # Luodaan ja käynnistetään ajastinolio käyttöjärjestelmän luokasta
+        $Timer =  [system.diagnostics.stopwatch]::StartNew()
+
+        # Käydään parametrina annetut koneet yksitellen läpi
+        foreach ($Machine in $ComputerName)
+            {
+                ScanNics -ComputerName $Machine # kutsutaan työfuntiota
+            }
+    }
+
+    # Lohko, joka suoritetaan työfunktion jälkeen
+    END 
+    {
+        # Pysäytetään ajastin ja kerrotaan paljonko aikaa skannaukseen meni
+        $Timer.Stop()
+        $ElapsedTime = $Timer.Elapsed.TotalSeconds
+        Write-Warning "NIC Information scanned in $ElapsedTime seconds"
+    }
+}
---
+++ sovelmaSnippet.ps1
@@ -0,0 +1,68 @@
+﻿# Malline komentosovelmalle, joka luo uusia objekteja
+# Korvaa <> merkinnän sisällä olevat nimet tai määrittelyt
+
+# Luokkamääritys luotaville uusille objekteille
+class <LuokanNimi>
+{
+    [<tietotyyppi>]$<ominaisuus>
+}
+
+# Esittelyfunktio, jossa määritellään komentosovelman parametrit, näkyy ulospäin (public)
+function <komento-sovelma> # Nimi muodostetaan sääntöjen mukaan: Verbi-Objekti
+{
+    [CmdletBinding()] # Laajennettuparametrimäärittely käyttöön 
+    param
+    (
+        # Parametri on pakollinen ja  se voi saada arvonsa putkittamalla
+        [Parameter(Mandatory=1, ValueFromPipeline=1, ValueFromPipelineByPropertyName=1)]
+        [<Tietotyyppi>]$<ParametrinNimi> # Huom! yleensä vektori, johon voi tallentaa useita arvoja, esim. [string[]]$ComputerName
+    )
+
+    # Lohko, jossa määritellään ulospäin näkymätön (private) työfunktio
+    BEGIN 
+    {
+        # Työfunktio, jonka avulla haetaan tarvittavat tiedot ja luodaan uusi objekti
+        function <työfunktio>
+        {
+            # Työfunktion parametrit
+            param
+            (
+                [<Tietotyyppi>]$<ParametrinNimi>
+            )
+
+            # Tyhjä vektori tuloksia varten
+            $Output = @()
+
+            # Uuden olion muodostaminen
+            $<olio> = <LuokanNimi>::new()
+            $<olio>.<ominaisuus> = <arvo>
+
+            # Lisätään objekti vektoriin
+            $Output = $Output + $<olio>
+ 
+            # Tulostetaan tiedot vektorista konsolille
+            Write-Output $Output
+        }
+   
+    # Lohko, jossa kutsutaan työfunktiota esittelyfunktion parametrina annetuissa objekteissa
+    PROCESS 
+    {
+        # Luodaan ja käynnistetään ajastinolio käyttöjärjestelmän luokasta
+        $Timer =  [system.diagnostics.stopwatch]::StartNew()
+
+        # Käydään parametrina annetut objektit yksitellen läpi
+        foreach ($<jäsen> in $<ParametrinNimi>)
+            {
+                <työfunktio> -<ParametrinNimi> $<jäsen> # kutsutaan työfuntiota
+            }
+    }
+
+    # Lohko, joka suoritetaan työfunktion jälkeen
+    END 
+    {
+        # Pysäytetään ajastin ja kerrotaan paljonko aikaa skannaukseen meni
+        $Timer.Stop()
+        $ElapsedTime = $Timer.Elapsed.TotalSeconds
+        Write-Warning "<ilmoitus teksti> $ElapsedTime seconds"
+    }
+}
